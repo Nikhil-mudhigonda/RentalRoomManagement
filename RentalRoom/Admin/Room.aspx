4af3c0457ab6d4a3499c30662f36e7c84b0a309f
Universe <Universe@DESKTOP-6D5R4T4>
2026-02-10 13:24:43 +0400

    Admin-Add,Delete

---
+++ RentalRoom/Admin/Room.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Room.aspx.cs" Inherits="RentalRoom.Admin.Room" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Label ID="RoomIbl" runat="server" Text="Room"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="RoomTextBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="BasePricelbl" runat="server" Text="BasePrice"></asp:Label>
+&nbsp;&nbsp;
+        <asp:TextBox ID="BasePriceTxtBox" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:Label ID="isActivelbl" runat="server" Text="isActive"></asp:Label>
+        <asp:RadioButtonList ID="isActiveRadioBtn" runat="server" BorderStyle="Solid" CellPadding="5" Height="57px" Width="187px">
+            <asp:ListItem Value="1">yes</asp:ListItem>
+            <asp:ListItem Value="0">no</asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <asp:Button ID="AddBtn" runat="server" OnClick="AddBtn_Click" Text="Add" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="DeleteId" runat="server" OnClick="DeleteId_Click1" Text="Delete" />
+        <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <br />
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateSelectButton="True" Height="239px" OnSelectedIndexChanged="GridView1_SelectedIndexChanged" Width="678px">
+        </asp:GridView>
+        <br />
+    </form>
+</body>
+</html>
---
+++ RentalRoom/Admin/Room.aspx.cs
@@ -0,0 +1,91 @@
+﻿using RentalRoom.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RentalRoom.Admin
+{
+    public partial class Room : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadRooms();
+            }
+        }
+
+        private void LoadRooms()
+        {
+            using (SqlConnection conn = DbHelper.GetConnection())
+            {
+                string query = "SELECT RoomId, Room_Name, Base_Price, isActive FROM Room";
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+            
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(GridView1.SelectedRow != null)
+            {
+                using (SqlConnection conn = DbHelper.GetConnection())
+                {
+                    string query = "SELECT RoomId, Room_Name, Base_Price, isActive FROM Room where RoomId = @Id";
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@Id", Convert.ToInt32(GridView1.SelectedRow.Cells[1].Text));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        RoomTextBox.Text = reader["Room_Name"].ToString();
+                        BasePriceTxtBox.Text = reader["Base_Price"].ToString();
+                        bool isActive = (bool)reader["isActive"];
+                        isActiveRadioBtn.SelectedValue = isActive ? "1":"0";
+                    }
+            }
+            }
+            
+        }
+
+        protected void AddBtn_Click(object sender, EventArgs e)
+        {
+            using(SqlConnection conn = DbHelper.GetConnection())
+            {
+                string query = "INSERT INTO Room (Room_Name, Base_Price, isActive) VALUES (@RoomName, @BasePrice, @IsActive)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@RoomName", RoomTextBox.Text);
+                cmd.Parameters.AddWithValue("@BasePrice", decimal.Parse(BasePriceTxtBox.Text));
+                cmd.Parameters.AddWithValue("@IsActive", isActiveRadioBtn.SelectedValue);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            LoadRooms();
+        }
+
+        protected void DeleteId_Click1(object sender, EventArgs e)
+        {
+            using (SqlConnection conn = DbHelper.GetConnection())
+            {
+                string query = "delete from Room where RoomId = @id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", Convert.ToInt32(GridView1.SelectedRow.Cells[1].Text));
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            LoadRooms();
+        }
+    }
+}
